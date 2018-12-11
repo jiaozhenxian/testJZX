a401acde5841772105fcfbe8892d03d925828657
焦振先 <565594904@qq.com>
2018-12-11 14:51:23 +0800

    初始化项目

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 焦振先 on 2018/12/11.
+//  Copyright © 2018 JZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 焦振先 on 2018/12/11.
+//  Copyright © 2018 JZX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
